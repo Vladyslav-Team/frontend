5ed25a10b4489771417c7b051d38bd07cb528fba
Oleksandra Hryshchenko <galexa0320@gmail.com>
2023-03-22 08:29:24 +0000

    Header+logo comp implemented #4

--- src/App.js
+++ src/App.js
@@ -1,11 +1,13 @@
 import React from "react";
-   
+import { Header } from './components/header/Header';
+
 const App = () => {
   return (
     <div>
+      <Header />
       <p>SkillScope</p>
     </div>
   );
-};
+}
 
 export default App;
