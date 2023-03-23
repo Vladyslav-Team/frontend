77045a052595b32601e0306043ef054a4cab3f45
Kirill Serebrykov <kirill.baza22@gmail.com>
2023-03-23 12:26:30 +0100

    finish task commit #9

--- src/shared/api/services/axiosInstances.js
+++
@@ -1,6 +0,0 @@
-import axios from "axios";
-
-const BASE_URL = process.env.REACT_APP_API_URL;
-export const axiosInstance = axios.create({
-  baseURL: BASE_URL,
-});
--- src/shared/api/services/getTalent.js
+++
@@ -1,6 +0,0 @@
-import { axiosInstance } from "./axiosInstances";
-import { Endpoints } from "../constants/endpoints";
-export const getTalent = async () => {
-  const response = await axiosInstance.get(Endpoints.GET_ALL_TALENTS);
-  return response.data;
-};
