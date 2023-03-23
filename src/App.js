d9987082d702f0f0ab7617788ae3a768a751d6da
Kirill Serebrykov <kirill.baza22@gmail.com>
2023-03-23 19:33:40 +0100

    use space #29

--- src/App.js
+++ src/App.js
@@ -2,17 +2,17 @@ import MockAdapter from "axios-mock-adapter"
 import axios from "axios"
 import {allTalentsResponse} from "./response/allTalents"
 import {
-	authenticationResponse,
-	authenticationResponseError,
+    authenticationResponse,
+    authenticationResponseError,
 } from "./response/authentication"
 import {Endpoints} from "../constants/endpoints"
 
 const mock = new MockAdapter(axios)
 export const MockInit = () => {
-	mock.onGet("/talents").reply(200, allTalentsResponse)
-	mock.onGet(Endpoints.GET_TALENT_BY_ID, {params: {"talent-id": "1"}}).reply(200, {})
-	mock.onPost(Endpoints.POST_TALENT_ADD).reply(200, authenticationResponse)
-	mock.onPost(Endpoints.POST_TALENT_LOGIN).reply(500, authenticationResponseError)
-	mock.onPatch(Endpoints.PATCH_TALENT_INFO, {params: {"talent-id": "1"}}).reply(200, {})
-	mock.onDelete(Endpoints.DELETE_TALENT, {params: {"talent-id": "1"}}).reply(200, {})
+    mock.onGet("/talents").reply(200, allTalentsResponse)
+    mock.onGet(Endpoints.GET_TALENT_BY_ID, {params: {"talent-id": "1"}}).reply(200, {})
+    mock.onPost(Endpoints.POST_TALENT_ADD).reply(200, authenticationResponse)
+    mock.onPost(Endpoints.POST_TALENT_LOGIN).reply(500, authenticationResponseError)
+    mock.onPatch(Endpoints.PATCH_TALENT_INFO, {params: {"talent-id": "1"}}).reply(200, {})
+    mock.onDelete(Endpoints.DELETE_TALENT, {params: {"talent-id": "1"}}).reply(200, {})
 }
