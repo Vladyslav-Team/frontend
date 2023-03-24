4136d1bbb09e1a98a818fb3d1990f080af5b5171
Kirill Serebrykov <kirill.baza22@gmail.com>
2023-03-24 16:21:10 +0100

    return header in app.jsx #9

--- src/App.jsx
+++ src/App.jsx
@@ -1,7 +1,9 @@
 import React from "react"
+import {Header} from "./components/Header"
 const App = () => {
     return (
         <div>
+            <Header />
             <p>SkillScope</p>
         </div>
     )
