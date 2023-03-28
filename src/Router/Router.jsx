db218aa4ecd5f5da17ef0d492ee611bd5d4acf17
VladislavZabilskij <zabilskijvladislav@gmail.com>
2023-03-28 18:23:22 +0300

    2 Login Popup #23

--- src/Router/Router.jsx
+++ src/Router/Router.jsx
@@ -1,6 +1,7 @@
 import React from "react"
 import {Route, Routes} from "react-router-dom"
 import {CardsList} from "../components/CardsList"
+import {LoginPopup} from "../components/LoginPopup"
 import {Endpoints} from "../shared/api/constants/endpoints"
 
 const Router = () => {
@@ -9,10 +10,7 @@ const Router = () => {
             <Route path={`${Endpoints.GET_ALL_TALENTS}`}>
                 <Route index element={<CardsList />} />
                 <Route path={"signup"} element={<h1>Sign up</h1>} />
-                <Route
-                    path={`${Endpoints.POST_TALENT_LOGIN}`}
-                    element={<h1>Log in</h1>}
-                />
+                <Route path={`${Endpoints.POST_TALENT_LOGIN}`} element={<LoginPopup />} />
             </Route>
             <Route path="*" element={<h1>Error Page</h1>} />
         </Routes>
--- src/components/LoginPopup/LoginPopup.jsx
+++ src/components/LoginPopup/LoginPopup.jsx
@@ -1,16 +1,21 @@
 import React, {useState} from "react"
 import {useForm} from "react-hook-form"
 import styles from "./LoginPopup.module.css"
+// import {registerOptions} from "../pages/SignUp/validationRules.js"
 
-const LoginPopup = (props) => {
-    const {register, handleSubmit} = useForm()
+const LoginPopup = ({isVisibility, setVisibility}) => {
+    const {
+        register,
+        handleSubmit,
+        formState: {errors},
+    } = useForm()
     const onSubmit = (data) => console.log(JSON.stringify(data))
 
     return (
         <>
             <div
                 className={styles.login_background}
-                onClick={() => props.loginFormVisibility()}
+                // onClick={() => 0}
             />
             <form className={styles.login_form} onSubmit={handleSubmit(onSubmit)}>
                 <h2 className={styles.login_form_elem}>Log in</h2>
@@ -18,27 +23,21 @@ const LoginPopup = (props) => {
                     <label>Email</label>
                     <br />
                     <input
-                        type="email"
-                        {...register("loginEmail", {
-                            required: true,
-                            minLength: 5,
-                            maxLength: 254,
-                        })}
+                        type="text"
+                        {...register("loginEmail", {required: true})} //registerOptions.email
                         className={styles.login_form_elem}
                     />
+                    {/* {errors.loginEmail && <p className={styles.error}>{errors.loginEmail.message}</p>} */}
                 </div>
                 <div className={styles.login_form_elem}>
                     <label>Password</label>
                     <br />
                     <input
                         type="password"
-                        {...register("loginPassword", {
-                            required: true,
-                            minLength: 8,
-                            maxLength: 64,
-                        })}
+                        {...register("loginPassword", {required: true})} //registerOptions.password.required, registerOptions.password.minLength, registerOptions.password.maxLength
                         className={styles.login_form_elem}
                     />
+                    {/* {errors.loginPassword && <p className={styles.error}>{errors.loginPassword.message}</p>} */}
                 </div>
                 <button className={styles.login_form_elem} type="submit">
                     LOG IN
