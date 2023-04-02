11c52302e2180a164e6322add8ee9fd5155e8e98
Kirill Serebrykov <kirill.baza22@gmail.com>
2023-04-02 16:42:09 +0200

    fix 404 page #58

---
+++ src/components/404/PageNotFound.jsx
@@ -0,0 +1,26 @@
+import React from "react"
+import {ReactComponent as PageNotFoundSVG} from "../../source/img/PageNotFound.svg"
+import Grid from "@mui/material/Grid"
+import useMediaQuery from "@mui/material/useMediaQuery"
+const PageNotFound = () => {
+    const matches = useMediaQuery("(max-width:832px)")
+    return (
+        <Grid
+            container
+            width={"100%"}
+            height={"100vh"}
+            justifyContent={"center"}
+            alignItems={"center"}
+            flexDirection={"column"}
+            paddingBottom={"20px"}>
+            <Grid item marginBottom={"60px"}>
+                <PageNotFoundSVG
+                    style={{transform: !matches ? "scale(2)" : "scale(1)"}}
+                />
+            </Grid>
+            <Grid item>{!matches && <h1>Page not found</h1>}</Grid>
+        </Grid>
+    )
+}
+
+export {PageNotFound}
